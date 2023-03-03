a90e030c67e7b20b6969bec6e7f49fd2aede3a10
AngeloDima <Angelodimauro91@gmail.com>
2023-03-03 12:29:11 +0100

    Create page

--- Desktop/JavaEs Course/FotoAlbum/src/main/java/com/example/demo/Controller/fotoController.java
+++ Desktop/JavaEs Course/FotoAlbum/src/main/java/com/example/demo/Controller/fotoController.java
@@ -5,12 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.Model.foto;
 import com.example.demo.Repository.fotoRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/")
@@ -37,5 +42,30 @@ public class fotoController {
     	return "dettagli";
     	
     }
+    
+    
+    
+    
+ 
+    
+    @GetMapping("/create")
+    public String create(Model model) {
+        foto createFoto = new foto();
+        model.addAttribute("createFoto", createFoto);
+        return "creaFoto";
+    }
+
+    @PostMapping("/create")
+    public String storeCreate(@ModelAttribute("createFoto") @Valid foto formFoto,BindingResult bindingResult, Model model) {
+ {
+        
+
+        repository.save(formFoto);
+        return "redirect:/";
+    }
+    
 	
-}
+    }}
+
+
+
